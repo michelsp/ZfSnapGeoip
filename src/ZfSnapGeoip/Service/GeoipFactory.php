12e61824f46578027e3d8bed0c41e16ae6c8c068
michel <michelspintor@gmail.com>
2016-07-21 16:54:32 -0300

    Factory

--- src/ZfSnapGeoip/Service/GeoipFactory.php
+++ src/ZfSnapGeoip/Service/GeoipFactory.php
@@ -1,12 +1,11 @@
 <?php
+
 namespace ZfSnapGeoip\Service;
+
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\Http\PhpEnvironment\Request as HttpRequest;
-<<<<<<< HEAD
-=======
 
->>>>>>> 2a9a4ba49931d5d5bcee8156178f417da7f2340e
 /**
  * Factory of Geoip
  *
@@ -14,16 +13,15 @@ use Zend\Http\PhpEnvironment\Request as HttpRequest;
  */
 class GeoipFactory implements FactoryInterface
 {
+
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $record = $serviceLocator->get('geoip_record');
         $hydrator = $serviceLocator->get('geoip_hydrator');
         $config = $serviceLocator->get('ZfSnapGeoip\DatabaseConfig');
         $request = new HttpRequest();
-<<<<<<< HEAD
-=======
 
->>>>>>> 2a9a4ba49931d5d5bcee8156178f417da7f2340e
         return new Geoip($record, $hydrator, $config, $request);
     }
+
 }
